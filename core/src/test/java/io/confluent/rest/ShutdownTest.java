0f4e91c70946df222b9181db3607b020687aa442
Ewen Cheslack-Postava <me@ewencp.org>
2014-12-29 16:28:21 -0800

    Add graceful shutdown and a shutdown hook in Application.
    The graceful shutdown period, during which new requests are rejected but
    outstanding requests are allowed to finish, is configurable. A shutdown hook in
    Application also allows additional resources not associated with any single
    request to be cleaned up. In order to ensure the process does not exit before
    the hook runs, lifecycle methods are added to Application that wrap and augment
    the ones in Jetty's Server class.

---
+++ core/src/test/java/io/confluent/rest/ShutdownTest.java
@@ -0,0 +1,149 @@
+/**
+ * Copyright 2014 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package io.confluent.rest;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Configurable;
+
+import static org.junit.Assert.*;
+
+public class ShutdownTest {
+  @Test
+  public void testShutdownHook() throws Exception {
+    Properties props = new Properties();
+    props.put("shutdown.graceful.ms", "50");
+    ShutdownApplication app = new ShutdownApplication(new RestConfig(props));
+    app.start();
+
+    StopThread stop = new StopThread(app);
+    stop.start();
+
+    app.join();
+    assertTrue(app.shutdown.get());
+  }
+
+  @Test
+  public void testGracefulShutdown() throws Exception {
+    Properties props = new Properties();
+    props.put("shutdown.graceful.ms", "50");
+    final RestConfig config = new RestConfig(props);
+    ShutdownApplication app = new ShutdownApplication(config);
+    app.start();
+
+    RequestThread req = new RequestThread(config);
+    req.start();
+    app.resource.requestProcessingStarted.await();
+
+    StopThread stop = new StopThread(app);
+    stop.start();
+
+    app.join();
+
+    assertTrue(req.finished);
+    assertEquals("done", req.response);
+  }
+
+
+  private static class ShutdownApplication extends Application<RestConfig> {
+    public AtomicBoolean shutdown = new AtomicBoolean(false);
+    public SlowResource resource = new SlowResource();
+
+    ShutdownApplication(RestConfig props) {
+      super(props);
+    }
+
+    @Override
+    public void onShutdown() {
+      shutdown.set(true);
+    }
+
+    @Override
+    public void setupResources(Configurable<?> config, RestConfig appConfig) {
+      config.register(resource);
+    }
+  }
+
+  @Path("/")
+  public static class SlowResource {
+    public CountDownLatch requestProcessingStarted = new CountDownLatch(1);
+
+    @GET
+    public String test() throws InterruptedException {
+      requestProcessingStarted.countDown();
+      Thread.sleep(25);
+      return "done";
+    }
+  }
+
+  private static class StopThread extends Thread {
+    ShutdownApplication app;
+
+    StopThread(ShutdownApplication app) {
+      this.app = app;
+    }
+
+    @Override
+    public void run() {
+      try {
+        app.stop();
+      } catch (Exception e) {
+      }
+    }
+  };
+
+  private static class RequestThread extends Thread {
+    RestConfig config;
+    boolean finished = false;
+    String response = null;
+
+    RequestThread(RestConfig config) {
+      this.config = config;
+    }
+    @Override
+    public void run() {
+      // It seems that the server isn't necessarily listening when start() returns, which makes it
+      // difficult to know when it is safe to make this request. Just retry until we're able to make
+      // the request.
+      while(true) {
+        try {
+          Client client = ClientBuilder.newClient();
+          response = client
+              .target("http://localhost:" + config.getInt(RestConfig.PORT_CONFIG))
+              .path("/")
+              .request()
+              .get(String.class);
+          finished = true;
+          return;
+        } catch (javax.ws.rs.ProcessingException e) {
+          // ignore and retry
+        }
+      }
+    }
+  };
+
+}
