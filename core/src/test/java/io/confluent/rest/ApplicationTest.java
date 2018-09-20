0ea3b3a71da535c2cf6a1b7dc3fd7dfcde95db02
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2018-09-20 21:00:07 +0100

    Add concept of Resource Extensions (#119)
    * Add resource extensions

--- core/src/test/java/io/confluent/rest/ApplicationTest.java
+++ core/src/test/java/io/confluent/rest/ApplicationTest.java
@@ -16,24 +16,48 @@
 
 package io.confluent.rest;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.http.HttpStatus.Code;
 import org.eclipse.jetty.security.ConstraintMapping;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.security.Constraint;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Configurable;
+import javax.ws.rs.core.MediaType;
 
 import io.confluent.common.config.ConfigException;
+import io.confluent.rest.extention.ResourceExtension;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -43,26 +67,27 @@ import static org.junit.Assert.assertTrue;
 
 public class ApplicationTest {
 
-  public static final String REALM = "realm";
+  private static final String REALM = "realm";
 
-  TestApplication application;
-
-  public class TestApplication extends Application<RestConfig> {
-
-    public TestApplication(RestConfig config) {
-      super(config);
-    }
-
-    @Override
-    public void setupResources(Configurable<?> config, RestConfig appConfig) {
-
-    }
-  }
+  @Rule
+  public final ExpectedException expectedException = ExpectedException.none();
 
   @Before
-  public void setup() {
-    Properties props = new Properties();
-    application = new TestApplication(new TestRestConfig(props));
+  public void setUp() {
+    TestApp.SHUTDOWN_CALLED.set(false);
+    TestRegistryExtension.CLOSE_CALLED.set(false);
+  }
+
+  private TestApp application;
+
+  @Before
+  public void setup() throws Exception {
+    application = new TestApp();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    application.stop();
   }
 
   @Test
@@ -89,7 +114,7 @@ public class ApplicationTest {
   public void testParseListenersUnparseableUri() {
     List<String> listenersConfig = new ArrayList<String>();
     listenersConfig.add("!");
-    List<URI> listeners = Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
+    Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
   }
 
   @Test
@@ -107,14 +132,14 @@ public class ApplicationTest {
     List<String> listenersConfig = new ArrayList<String>();
     listenersConfig.add("foo://localhost:8080");
     listenersConfig.add("bar://localhost:8081");
-    List<URI> listeners = Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
+    Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
   }
 
   @Test(expected = ConfigException.class)
   public void testParseListenersNoPort() {
     List<String> listenersConfig = new ArrayList<String>();
     listenersConfig.add("http://localhost");
-    List<URI> listeners = Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
+    Application.parseListeners(listenersConfig, -1, Arrays.asList("http", "https"), "http");
   }
 
   @Test
@@ -170,7 +195,7 @@ public class ApplicationTest {
     final List<String> roles = Arrays.asList("roleA", "roleB");
     ConstraintSecurityHandler securityHandler = application.createBasicSecurityHandler(REALM, roles);
     servletContextHandler.setSecurityHandler(securityHandler);
-    setAndAssertUnsecureConstraints(servletContextHandler, securityHandler, 3);
+    setAndAssertUnsecuredConstraints(servletContextHandler, securityHandler, 3);
   }
 
   @Test
@@ -179,7 +204,7 @@ public class ApplicationTest {
     final List<String> roles = Arrays.asList("roleA", "roleB");
     ConstraintSecurityHandler securityHandler = application.createBasicSecurityHandler(REALM, roles);
     servletContextHandler.setSecurityHandler(securityHandler);
-    setAndAssertUnsecureConstraints(servletContextHandler, securityHandler, 1);
+    setAndAssertUnsecuredConstraints(servletContextHandler, securityHandler, 1);
   }
 
   @Test
@@ -188,7 +213,7 @@ public class ApplicationTest {
     final List<String> roles = Arrays.asList("roleA", "roleB");
     ConstraintSecurityHandler securityHandler = application.createBasicSecurityHandler(REALM, roles);
     servletContextHandler.setSecurityHandler(securityHandler);
-    setAndAssertUnsecureConstraints(servletContextHandler, securityHandler, 0);
+    setAndAssertUnsecuredConstraints(servletContextHandler, securityHandler, 0);
   }
 
   @Test
@@ -196,10 +221,38 @@ public class ApplicationTest {
     ServletContextHandler servletContextHandler  = new ServletContextHandler();
     Application.setUnsecurePathConstraints(servletContextHandler, Arrays.asList("/path1"));
     assertNull(servletContextHandler.getSecurityHandler());
-
   }
 
-  private void setAndAssertUnsecureConstraints(
+  @Test
+  public void shouldInitializeResourceExtensions()throws Exception {
+    try (TestApp testApp = new TestApp(TestRegistryExtension.class)) {
+      assertThat(makeGetRequest(testApp, "/custom/resource"), is(Code.OK));
+    }
+  }
+
+  @Test
+  public void shouldThrowIfResourceExtensionThrows() throws Exception {
+    expectedException.expectMessage(
+        containsString("Exception throw by resource extension. ext:"));
+
+    new TestApp(TestRegistryExtension.class, BadRegistryExtension.class);
+  }
+
+  @Test
+  public void shouldCloseResourceExtensions() throws Exception{
+    new TestApp(TestRegistryExtension.class).stop();
+    assertThat("close called", TestRegistryExtension.CLOSE_CALLED.get(), is(true));
+  }
+
+  @Test
+  public void shouldShutdownProperlyEvenIfResourceExtensionThrowsOnShutdown() throws Exception {
+    final TestApp testApp = new TestApp(UnstoppableRegistryExtension.class);
+    testApp.stop();
+    testApp.join();
+    assertThat("shutdown called", TestApp.SHUTDOWN_CALLED.get(), is(true));
+  }
+
+  private void setAndAssertUnsecuredConstraints(
       ServletContextHandler servletContextHandler,
       ConstraintSecurityHandler securityHandler,
       int numPaths
@@ -226,5 +279,119 @@ public class ApplicationTest {
     assertEquals("Scheme should be " + scheme, scheme, uri.getScheme());
     assertEquals("Host should be " + host, host, uri.getHost());
     assertEquals("Port should be " + port, port, uri.getPort());
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private HttpStatus.Code makeGetRequest(final TestApp app, final String path) throws Exception {
+    final HttpGet httpget = new HttpGet(app.getListeners().get(0).toString() + path);
+
+    try (CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = httpClient.execute(httpget)) {
+      return HttpStatus.getCode(response.getStatusLine().getStatusCode());
+    }
+  }
+
+  private static class TestApp extends Application<TestRestConfig> implements AutoCloseable {
+    private static final AtomicBoolean SHUTDOWN_CALLED = new AtomicBoolean(true);
+
+    @SafeVarargs
+    private TestApp(final Class<? extends ResourceExtension>... extensions) throws Exception {
+      super(createConfig(extensions));
+
+      start();
+    }
+
+    @Override
+    public void setupResources(final Configurable<?> config, final TestRestConfig appConfig) {
+    }
+
+    @Override
+    public void close() throws Exception {
+      stop();
+    }
+
+    @Override
+    public void onShutdown() {
+      SHUTDOWN_CALLED.set(true);
+    }
+
+    private List<URL> getListeners() {
+      return Arrays.stream(server.getConnectors())
+          .filter(connector -> connector instanceof ServerConnector)
+          .map(ServerConnector.class::cast)
+          .map(connector -> {
+            try {
+              final String protocol = new HashSet<>(connector.getProtocols())
+                  .stream()
+                  .map(String::toLowerCase)
+                  .anyMatch(s -> s.equals("ssl")) ? "https" : "http";
+
+              final int localPort = connector.getLocalPort();
+
+              return new URL(protocol, "localhost", localPort, "");
+            } catch (final Exception e) {
+              throw new RuntimeException("Malformed listener", e);
+            }
+          })
+          .collect(Collectors.toList());
+    }
+
+    @SafeVarargs
+    private static TestRestConfig createConfig(final Class<? extends ResourceExtension>... exts) {
+      final String extensionList = Arrays.stream(exts)
+          .map(Class::getName)
+          .collect(Collectors.joining(","));
+
+      final HashMap<String, Object> props = new HashMap<>();
+      props.put(RestConfig.LISTENERS_CONFIG, "http://0.0.0.0:0");
+      props.put(RestConfig.RESOURCE_EXTENSION_CLASSES_CONFIG, extensionList);
+      return new TestRestConfig(props);
+    }
+  }
+
+  @Path("/custom")
+  @Produces(MediaType.TEXT_PLAIN)
+  public static class RestResource {
+    @GET
+    @Path("/resource")
+    public String get() {
+      return "Hello";
+    }
+  }
+
+  public static class TestRegistryExtension implements ResourceExtension<TestApp> {
+    private static final AtomicBoolean CLOSE_CALLED = new AtomicBoolean(true);
+
+    @Override
+    public void register(final Configurable<?> config, final TestApp app) {
+      final TestRestConfig restConfig = app.getConfiguration();
+      assertNotNull(restConfig);
+
+      config.register(new RestResource());
+    }
+
+    @Override
+    public void close() {
+      CLOSE_CALLED.set(true);
+    }
+  }
+
+  public static class BadRegistryExtension implements ResourceExtension<Application<?>> {
+    @Override
+    public void register(final Configurable<?> config, final Application<?> app) {
+      throw new IllegalArgumentException("Boom");
+    }
+  }
+
+  public static class UnstoppableRegistryExtension implements ResourceExtension<Application<?>> {
+
+    @Override
+    public void register(final Configurable<?> config, final Application<?> app) {
+    }
+
+    @Override
+    public void close() throws IOException {
+      throw new IOException("Boom");
+    }
   }
 }
