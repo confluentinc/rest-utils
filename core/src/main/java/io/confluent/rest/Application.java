5912f60e6851160650be69367490a10716f920ed
Kostya Linou <klinou@confluent.io>
2021-10-15 21:02:00 +0100

    Use jakarta.servlet.DispatcherType.

--- core/src/main/java/io/confluent/rest/Application.java
+++ core/src/main/java/io/confluent/rest/Application.java
@@ -68,8 +68,8 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.ServletException;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.ServletException;
 import jakarta.ws.rs.core.Configurable;
 
 import io.confluent.common.config.ConfigException;
