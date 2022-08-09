8286bb304ff88a80078e47c4f2cc1b82591fd8fa
Jing Liu <jl5311@nyu.edu>
2022-08-09 14:54:26 -0700

    Add JsonParseExceptionMapper and unit test for it

--- core/src/main/java/io/confluent/rest/Application.java
+++ core/src/main/java/io/confluent/rest/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Confluent Inc.
+ * Copyright 2014 - 2022 Confluent Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package io.confluent.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.jaxrs.base.JsonParseExceptionMapper;
 
 import io.confluent.rest.errorhandlers.NoJettyDefaultStackTraceErrorHandler;
 import org.apache.kafka.common.config.ConfigException;
@@ -84,6 +83,7 @@ import io.confluent.rest.exceptions.ConstraintViolationExceptionMapper;
 import io.confluent.rest.exceptions.GenericExceptionMapper;
 import io.confluent.rest.exceptions.WebApplicationExceptionMapper;
 import io.confluent.rest.exceptions.JsonMappingExceptionMapper;
+import io.confluent.rest.exceptions.JsonParseExceptionMapper;
 import io.confluent.rest.extension.ResourceExtension;
 import io.confluent.rest.filters.CsrfTokenProtectionFilter;
 import io.confluent.rest.metrics.MetricsResourceMethodApplicationListener;
@@ -650,6 +650,7 @@ public abstract class Application<T extends RestConfig> {
   protected void registerExceptionMappers(Configurable<?> config, T restConfig) {
     config.register(ConstraintViolationExceptionMapper.class);
     config.register(JsonMappingExceptionMapper.class);
+    config.register(JsonParseExceptionMapper.class);
     config.register(new WebApplicationExceptionMapper(restConfig));
     config.register(new GenericExceptionMapper(restConfig));
   }
