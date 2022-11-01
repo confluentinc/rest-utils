fc1bd3698586f10b29100c75aa0248cef59a83da
Rigel Bezerra de Melo <rigelbm@confluent.io>
2022-11-01 14:58:14 +0000

    APIF-2959: Prepare rest-utils API for per-listener SSL configs. (#357)
    This PR prepares the rest-utils API for per-listener SSL configs. Changes:
    
        [new] SslConfig: This class encapsulates the many SSL params in rest-utils.
        [new] SslFactory: ApplicationServer.createSslContextFactory method extracted out.
        Moved all the listener parsing logic from ApplicationServer to RestConfig.
        [new] RestConfig.getBaseSslConfig(): Returns the unprefixed (base) SSL configs.
        [new] RestConfig.getSslConfigs(): Returns the per-listener SSL configs.
    
    Note that this PR does not implements per-listener SSL config. It only introduces the APIs, so that downstream applications can adapt before the changes. As such, all the SSL configs returned are the same for all listeners.

--- core/src/main/java/io/confluent/rest/Application.java
+++ core/src/main/java/io/confluent/rest/Application.java
@@ -16,9 +16,40 @@
 
 package io.confluent.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static io.confluent.rest.RestConfig.REST_SERVLET_INITIALIZERS_CLASSES_CONFIG;
+import static io.confluent.rest.RestConfig.WEBSOCKET_SERVLET_INITIALIZERS_CLASSES_CONFIG;
+import static java.util.Collections.emptyMap;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.confluent.rest.auth.AuthUtil;
 import io.confluent.rest.errorhandlers.NoJettyDefaultStackTraceErrorHandler;
+import io.confluent.rest.exceptions.ConstraintViolationExceptionMapper;
+import io.confluent.rest.exceptions.GenericExceptionMapper;
+import io.confluent.rest.exceptions.WebApplicationExceptionMapper;
+import io.confluent.rest.exceptions.JsonMappingExceptionMapper;
+import io.confluent.rest.exceptions.JsonParseExceptionMapper;
+import io.confluent.rest.extension.ResourceExtension;
+import io.confluent.rest.filters.CsrfTokenProtectionFilter;
+import io.confluent.rest.metrics.MetricsResourceMethodApplicationListener;
+import io.confluent.rest.validation.JacksonMessageBodyProvider;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.ws.rs.core.Configurable;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.metrics.JmxReporter;
 import org.apache.kafka.common.metrics.MetricConfig;
@@ -56,41 +87,6 @@ import org.glassfish.jersey.server.validation.ValidationFeature;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.ws.rs.core.Configurable;
-
-import io.confluent.rest.auth.AuthUtil;
-import io.confluent.rest.exceptions.ConstraintViolationExceptionMapper;
-import io.confluent.rest.exceptions.GenericExceptionMapper;
-import io.confluent.rest.exceptions.WebApplicationExceptionMapper;
-import io.confluent.rest.exceptions.JsonMappingExceptionMapper;
-import io.confluent.rest.exceptions.JsonParseExceptionMapper;
-import io.confluent.rest.extension.ResourceExtension;
-import io.confluent.rest.filters.CsrfTokenProtectionFilter;
-import io.confluent.rest.metrics.MetricsResourceMethodApplicationListener;
-import io.confluent.rest.validation.JacksonMessageBodyProvider;
-
-import static io.confluent.rest.RestConfig.REST_SERVLET_INITIALIZERS_CLASSES_CONFIG;
-import static io.confluent.rest.RestConfig.WEBSOCKET_SERVLET_INITIALIZERS_CLASSES_CONFIG;
 
 /**
  * A REST application. Extend this class and implement setupResources() to register REST
@@ -499,10 +495,10 @@ public abstract class Application<T extends RestConfig> {
     // Support for named listeners is only implemented for the case of Applications
     // managed by ApplicationServer (direct instantiation of Application is to be
     // deprecated).
-    return ApplicationServer.parseListeners(
-      listenersConfig, Collections.emptyMap(), deprecatedPort, supportedSchemes, defaultScheme)
+    return RestConfig.parseListeners(
+            listenersConfig, emptyMap(), deprecatedPort, supportedSchemes, defaultScheme)
         .stream()
-        .map(ApplicationServer.NamedURI::getUri)
+        .map(NamedURI::getUri)
         .collect(Collectors.toList());
   }
 
