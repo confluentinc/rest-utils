f136a6a6b1173ca05068dc5c3d9a31aec4c5d5d5
Truc Nguyen <119303279+trnguyencflt@users.noreply.github.com>
2023-04-13 14:46:23 +0100

    Backport commit 37b9c48db285c3c0954724ce08a3a9f5546a158a for duplicate request log fix (#389)

--- core/src/main/java/io/confluent/rest/Application.java
+++ core/src/main/java/io/confluent/rest/Application.java
@@ -18,6 +18,7 @@ package io.confluent.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.metrics.JmxReporter;
 import org.apache.kafka.common.metrics.MetricConfig;
@@ -122,6 +123,11 @@ public abstract class Application<T extends RestConfig> {
   }
 
   public Application(T config, String path, String listenerName) {
+    this(config, path, listenerName, null);
+  }
+
+  @VisibleForTesting
+  Application(T config, String path, String listenerName, CustomRequestLog customRequestLog) {
     this.config = config;
     this.path = Objects.requireNonNull(path);
     this.listenerName = listenerName;
@@ -129,11 +135,16 @@ public abstract class Application<T extends RestConfig> {
     this.metrics = configureMetrics();
     this.getMetricsTags().putAll(config.getMap(RestConfig.METRICS_TAGS_CONFIG));
 
-    Slf4jRequestLogWriter logWriter = new Slf4jRequestLogWriter();
-    logWriter.setLoggerName(config.getString(RestConfig.REQUEST_LOGGER_NAME_CONFIG));
+    if (customRequestLog == null) {
+      Slf4jRequestLogWriter logWriter = new Slf4jRequestLogWriter();
+      logWriter.setLoggerName(config.getString(RestConfig.REQUEST_LOGGER_NAME_CONFIG));
 
-    // %{ms}T logs request time in milliseconds
-    requestLog = new CustomRequestLog(logWriter, CustomRequestLog.EXTENDED_NCSA_FORMAT + " %{ms}T");
+      // %{ms}T logs request time in milliseconds
+      requestLog = new CustomRequestLog(logWriter,
+          CustomRequestLog.EXTENDED_NCSA_FORMAT + " %{ms}T");
+    } else {
+      requestLog = customRequestLog;
+    }
   }
 
   public final String getPath() {
@@ -369,9 +380,10 @@ public abstract class Application<T extends RestConfig> {
 
     RequestLogHandler requestLogHandler = new RequestLogHandler();
     requestLogHandler.setRequestLog(requestLog);
+    context.insertHandler(requestLogHandler);
 
     HandlerCollection handlers = new HandlerCollection();
-    handlers.setHandlers(new Handler[]{context, requestLogHandler});
+    handlers.setHandlers(new Handler[]{context});
 
     return handlers;
   }
@@ -675,7 +687,7 @@ public abstract class Application<T extends RestConfig> {
         String.valueOf(config.getDosFilterMaxRequestsPerConnectionPerSec()));
     context.addFilter(filterHolder, "/*", EnumSet.of(DispatcherType.REQUEST));
   }
-  
+
   private void configureGlobalDosFilter(ServletContextHandler context) {
     DoSFilter dosFilter = new GlobalDosFilter();
     String globalLimit = String.valueOf(config.getDosFilterMaxRequestsGlobalPerSec());
