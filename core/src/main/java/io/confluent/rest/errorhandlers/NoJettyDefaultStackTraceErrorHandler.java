d626d6684cd46a0fdd5e7415a2f59999d93c9b07
Truc Nguyen <trnguyen@confluent.io>
2023-06-14 10:21:01 +0100

    Introduce custom response header and request attribute fields for custom error code

--- core/src/main/java/io/confluent/rest/errorhandlers/NoJettyDefaultStackTraceErrorHandler.java
+++ core/src/main/java/io/confluent/rest/errorhandlers/NoJettyDefaultStackTraceErrorHandler.java
@@ -4,6 +4,7 @@
 
 package io.confluent.rest.errorhandlers;
 
+import io.confluent.rest.CustomErrorCodeFields;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,11 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.ErrorHandler;
 
+/**
+ * This is the default error handler for all jetty errors, for example:
+ * - DoSFilter
+ * - SNI check from SSL connection
+ */
 public class NoJettyDefaultStackTraceErrorHandler extends ErrorHandler {
   public NoJettyDefaultStackTraceErrorHandler() {
     super();
@@ -23,6 +29,11 @@ public class NoJettyDefaultStackTraceErrorHandler extends ErrorHandler {
       HttpServletResponse response, int code, String message) throws IOException {
     // set Exception to null to avoid exposing stack trace to clients
     request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, null);
+    Object errorCode = request.getAttribute(CustomErrorCodeFields.CUSTOM_ERROR_CODE_REQUEST_ATTR);
+    if (errorCode instanceof Integer) {
+      response.setHeader(CustomErrorCodeFields.CUSTOM_ERROR_CODE_RESPONSE_HEADER,
+          String.valueOf(errorCode));
+    }
     super.generateAcceptableResponse(baseRequest, request, response, code,
         retrieveErrorMessage(code, message));
   }
