f7da104729495742824d333d49dd5d94f95a5516
Emma Humber <ehumber@confluent.io>
2024-06-03 14:50:05 +0100

    Checking if removing the comment helps this pass in the upstream build

--- core/src/test/java/io/confluent/rest/RequestLogHandlerIntegrationTest.java
+++ core/src/test/java/io/confluent/rest/RequestLogHandlerIntegrationTest.java
@@ -67,7 +67,7 @@ public class RequestLogHandlerIntegrationTest {
     httpClient.stop();
   }
 
-  @Disabled("KNET-15387: this test is flaky and needs to be fixed")
+  @Disabled
   @Test
   public void test_CustomRequestLog_registeredToCorrectListener() throws Exception {
     int internalPort = getFreePort();
