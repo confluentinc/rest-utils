5b9886b312c02537304b028025e01feee21180e4
Bob Barrett <bob.barrett@confluent.io>
2024-11-06 12:36:03 -0800

    Update log

--- core/src/main/java/io/confluent/rest/handlers/ExpectedSniHandler.java
+++ core/src/main/java/io/confluent/rest/handlers/ExpectedSniHandler.java
@@ -43,7 +43,7 @@ public class ExpectedSniHandler extends HandlerWrapper {
     if (sniServerName == null) {
       log.warn("No SNI header present on request");
     } else if (!expectedSniHeaders.contains(sniServerName)) {
-      log.warn("SNI header {} is not in the configured list of expected headers", sniServerName);
+      log.warn("SNI header {} is not in the configured list of expected headers {}", sniServerName, this.expectedSniHeaders);
     }
 
     super.handle(target, baseRequest, request, response);
