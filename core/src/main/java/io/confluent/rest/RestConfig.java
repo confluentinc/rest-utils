4d50834b991a6b2b2da09d0ad65ae4888de833ee
Nabeel Nauman <nnauman@confluent.io>
2025-08-08 16:40:53 +0100

    update description

--- core/src/main/java/io/confluent/rest/RestConfig.java
+++ core/src/main/java/io/confluent/rest/RestConfig.java
@@ -497,7 +497,7 @@ public class RestConfig extends AbstractConfig {
   private static final String DOS_FILTER_TENANT_ENABLED_CONFIG = "dos.filter.tenant.enabled";
   private static final String DOS_FILTER_TENANT_ENABLED_DOC =
       "Whether to enable per-tenant DoS filtering. This prevents noisy tenants from exhausting "
-          + "the global DoS filter. Default is true.";
+          + "the global DoS filter. Default is false.";
   private static final boolean DOS_FILTER_TENANT_ENABLED_DEFAULT = false;
 
   private static final String DOS_FILTER_TENANT_MAX_REQUESTS_PER_SEC_CONFIG =
