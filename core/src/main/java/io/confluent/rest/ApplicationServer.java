fb395dc721f76651809b97fe12a9192ae5c9e977
Silvan Li <sili@confluent.io>
2025-09-10 17:35:34 -0400

    fix build

--- core/src/main/java/io/confluent/rest/ApplicationServer.java
+++ core/src/main/java/io/confluent/rest/ApplicationServer.java
@@ -52,6 +52,7 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.NetworkTrafficServerConnector;
+import org.eclipse.jetty.server.ProxyConnectionFactory;
 import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.SslConnectionFactory;
